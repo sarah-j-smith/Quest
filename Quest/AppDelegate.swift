df41df9496cb13cbcaebbac8a850236696a572a5
Sarah Smith <sez@storybridge.org>
2015-01-11 17:10:00 +0000

    Initial Commit

---
+++ Quest/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Quest
+//
+//  Created by Sarah Smith on 11/01/2015.
+//  Copyright (c) 2015 Sarah Smith. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ QuestTests/QuestTests.swift
@@ -0,0 +1,36 @@
+//
+//  QuestTests.swift
+//  QuestTests
+//
+//  Created by Sarah Smith on 11/01/2015.
+//  Copyright (c) 2015 Sarah Smith. All rights reserved.
+//
+
+import Cocoa
+import XCTest
+
+class QuestTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
